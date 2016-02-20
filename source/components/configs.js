4836cbe18aca321f0a7d2ef4e5f9aea362c5c14a
bfreeds <brandynfriedly@gmail.com>
2016-02-20 11:53:33 -0500

    add baselayer control for gray and satellite

--- source/components/configs.js
+++ source/components/configs.js
@@ -1,28 +1,29 @@
-// Hidden implementation details
+// Variables local to this module
 const mbAccessToken = 'pk.eyJ1IjoiYmZyaWVkbHkiLCJhIjoiV2k5ZDRoZyJ9.suhdNFpeRtxt1OTpUVc2Tw';
+const mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mbAccessToken;
 
 const mapboxAttribution= 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
       '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="http://mapbox.com">Mapbox</a>';
 
-// Exported Object
+const grayscale = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mapboxAttribution});
+const satellite = L.tileLayer(mbUrl, {id: 'mapbox.satellite', attribution: mapboxAttribution});
+
+// Module exposes a single Object
 export default {
   leaflet: {
     center: [43.761921, -70.080350],
     zoom: 8,
     minZoom: 2,
-    maxzoom: 21
+    maxzoom: 21,
+    layers: [grayscale, /*,sites*/] //TODO:  define & add sites layer in in layer.js
     //TODO: maxbounds: []
   },
 
-  mapboxUrl: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mbAccessToken,
+  mapboxUrl: mbUrl,
 
-  grayBasemap: {
-    id: 'mapbox.light',
-    attribution: mapboxAttribution
-  },
-  satelliteBasemap: {
-    id: 'mapbox.earth',
-    attribution: mapboxAttribution
+  baselayers: {
+    'Grayscale': grayscale,
+    'Satellite': satellite
   }
 };
--- source/components/map.js
+++ source/components/map.js
@@ -1,16 +1,12 @@
 import configs from './configs';
 
-// Hidden implementation details
-const grayscale = L.tileLayer(configs.mapboxUrl, configs.grayBasemap);
-const satellite = L.tileLayer(configs.mapboxUrl, configs.satelliteBasemap);
-
-
-
-// Exported Function
+// Exposes single function
 export default function() {
   // Initialize Map Here
-  var map_object = L.map('Map', configs.leaflet)
-    grayscale.addTo(map_object);
+  var map_object = L.map('Map', configs.leaflet);
+
+  L.control.layers(configs.baselayers).addTo(map_object);
+
 
   //TODO:  add layer control
 
