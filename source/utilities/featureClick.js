a472eb4cca377f0d155456d7e3afa8811cbddee7
bfreeds <brandynfriedly@gmail.com>
2016-02-21 21:09:27 -0500

    log data to console

--- source/utilities/featureClick.js
+++ source/utilities/featureClick.js
@@ -1,6 +1,7 @@
 import d3 from 'd3'
 
 export default function(event, latlng, position, data) {
+  console.log(data)
   document.getElementById('sitename').innerHTML = data.sitename;
   document.getElementById('sitelocation').innerHTML = data.sitelocation;
   document.getElementById('sitedescription').innerHTML = data.sitedescription;
