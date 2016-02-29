08b757f8df6ac86898ea9a8c55c47ac4cf726a5b
bfreeds <brandynfriedly@gmail.com>
2016-02-28 22:05:51 -0500

    Get CartoDB data & add response to component state

--- source/js/components/map.jsx
+++ source/js/components/map.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import L from 'leaflet'
+import $ from 'axios'
 
 import config from './mapConfig';
 
@@ -16,14 +17,52 @@ const Map = React.createClass({
       //runs after map is added to the DOM
       console.log('Leaflet mounted on : ')
       console.log(ReactDOM.findDOMNode(this))
-      console.log(config.grayscale)
 
       const map_node = ReactDOM.findDOMNode(this)
+
       const map_object = L.map(map_node, config.leaflet)
 
+      console.log(this.state)
+
+      // Add Baselayer to Component State
       this.state.tileLayer = config.grayscale
       .addTo(map_object)
 
+      const query = 'SELECT * FROM ' + config.cartodb.table
+      $.get(config.cartodb.endpoint(config.cartodb.user, query))
+        .then(
+          function(response) {
+            const geojson = response.data
+            console.log(geojson)
+            this.setState({
+              dataLayer: geojson
+            })
+          }.bind(this)
+        )
+
+      // execute component method defined below
+      // this.getCdb();
+
+      // Did dataLayer get added to state? Nope.
+      console.log(this.state)
+  },
+
+  // TODO: hook this into a redux action, store data in the Redux Store
+    //This will let us perform one API Request on load, then refer to the Store for all other actions
+  getCdb() {
+    const query = 'SELECT * FROM ' + config.cartodb.table
+
+    $.get(config.cartodb.endpoint(config.cartodb.user, query))
+      .then(
+        function(res){
+          const geojson = res.data
+          console.log(geojson)
+          this.setState({
+            dataLayer: geojson
+          })
+          console.log(this.state)
+        }
+      )
   },
 
   //   //set state to include tile layer
--- source/js/components/mapConfig.js
+++ source/js/components/mapConfig.js
@@ -8,8 +8,13 @@ const mapboxAttribution= 'Map data &copy; <a href="http://openstreetmap.org">Ope
 const grayscale = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mapboxAttribution});
 const satellite = L.tileLayer(mbUrl, {id: 'mapbox.satellite', attribution: mapboxAttribution});
 
+const cartodbTable = 'table_2005_2012_data_sheet1'
+const cartodbUser = 'bfriedly'
+
+https://{account}.cartodb.com/api/v2/sql?q={SQL statement}
+
 // Module exposes a single Object
-export default {
+const configs = {
   leaflet: {
     center: [43.761921, -70.080350],
     zoom: 8,
@@ -18,13 +23,28 @@ export default {
     layer : [grayscale]
     //TODO: maxbounds: []
   },
-  cartodb_options: {
-    cartodb_logo: false,
-    legends: false,
-    https: true,
-    attributionControl: true
+  cartodb: {
+    endpoint: function(user, query) {
+      return (
+        'https://' +
+        user +
+        '.cartodb.com/api/v2/sql?format=GeoJSON&q=' +
+        query
+      )
+    },
+    user: 'bfriedly',
+    key: '89e600a074e47c8de1c727a59ce2786b1b1118e3',
+    table: cartodbTable,
+    layerOptions: {
+      cartodb_logo: false,
+      legends: false,
+      https: true,
+      attributionControl: true
+    }
   },
   mapboxUrl: mbUrl,
   grayscale: grayscale,
   satellite: satellite
 };
+
+export default configs
