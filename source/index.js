ce3610d5aaa4cf55655c9268d516c4f012338fd3
bfreeds <brandynfriedly@gmail.com>
2016-03-06 18:32:14 -0500

    move store logic to its own module

--- source/index.js
+++ source/index.js
@@ -1,11 +1,12 @@
+import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import cascoApp from 'reducers'
+
+import configureStore from 'store'
 import App from 'components/app'
 
-let store = createStore(cascoApp)
+const store = configureStore()
 
 render(
   <Provider store={store}>
--- source/js/store/index.js
+++ source/js/store/index.js
@@ -1,22 +1,24 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
-import cascoApp from 'reducers/reducers'
+import cascoApp from 'reducers'
 
 const loggerMiddleware = createLogger()
 // Making our store from our reducer!
-let store = createStore(
-  cascoApp,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
+export default function configureStore(initialState) {
+  return createStore(
+    cascoApp,
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware
+    )
   )
-)
+}
 
-// Immediately get our CartoDB data
-store.dispatch(
-  getCartodbData(
-    'bfriedly',
-    'SELECT * FROM table_2005_2012_data_sheet1'
-  )
-)
+// // Immediately get our CartoDB data
+// store.dispatch(
+//   getCartodbData(
+//     'bfriedly',
+//     'SELECT * FROM table_2005_2012_data_sheet1'
+//   )
+// )
