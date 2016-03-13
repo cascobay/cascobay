6ce02a2864da6b61417b8841f1da5469ff88c70f
bfreeds <brandynfriedly@gmail.com>
2016-03-12 19:34:12 -0500

    get this.props.cartodbData.geojson to render on leaflet map

--- source/js/components/map.jsx
+++ source/js/components/map.jsx
@@ -1,42 +1,77 @@
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom'
 import L from 'leaflet'
+import GJV from 'geojson-validation'
 
 import config from './mapConfig';
 
 const Map = React.createClass({
-  getInitialState () {
+  getInitialState() {
       return {
-        tileLayer: null
+        map: {},
+        tileLayer: null,
+        waterQualityLayer: null
       }
   },
+  componentDidMount() {
+    //runs after map is added to the DOM
+    // find out what dom element this component is mounted on, and instantiate our leaflet map onto that node
+    const map_node = ReactDOM.findDOMNode(this)
+    this.state.map = L.map(map_node, config.leaflet)
 
-  componentDidMount () {
-      //runs after map is added to the DOM
-      const map_node = ReactDOM.findDOMNode(this)
-      const map_object = L.map(map_node, config.leaflet)
+    // Add Baselayer to Component State
+    this.state.tileLayer = config.grayscale
+      .addTo(this.state.map)
 
-      console.log('Leaflet Instance mounted on: ', ReactDOM.findDOMNode(this))
-      console.log('state at beginning of componentDidMount: ', this.state)
-      console.log('props: ', this.props)
-
-      // Add Baselayer to Component State
-      this.state.tileLayer = config.grayscale
-      .addTo(map_object)
-
-      this.props.getCartodbData('bfriedly', 'SELECT * FROM table_2005_2012_data_sheet1')
-
-      // instantiate our geojson layer, will add data to layer in api request callback (data needs to be present first)
-      let waterQualityLayer = L.geoJson().addTo(map_object);
-
-      // add the data to the waterquality layer.
-      // QUESTION: will geojson be available as props by the time this component mounts?  if yes, just pass {this.props.geojson}.  if no, then we would pass the getCartodbData(username, query) action here
-      // waterQualityLayer.addData(this.props.cartodbData.geojson);
-
+    //redux's connect() method allows us to call action creators as component props
+    //this is equivalent to calling  dispatch(getCartodbData())
+    this.props.getCartodbData('bfriedly', 'SELECT * FROM table_2005_2012_data_sheet1')
   },
+  componentWillReceiveProps(nextProps) {
+    console.log('next props: ', nextProps)
+    //if the new props are a valid geojson object, add them to our leaflet map
+    if (nextProps.cartodbData.geojson !== this.props.cartodbData.geojson && GJV.valid(nextProps.cartodbData.geojson)) {
+      this.addGeojson(nextProps.cartodbData.geojson, this.props)
+    }
+  },
+  addGeojson(geojson, props) {
+    const geojsonStyle = {
+      radius: 8,
+      fillColor: '#ff7800',
+      color: '#000',
+      weight: 1,
+      opacity: 1,
+      filOpacity: 0.8
+    }
+    console.log('this.props.selectFeature: ', this.props.selectFeature)
 
+    function onClick(event){
+      const layer = event.target
 
-  render () {
+      console.log(props)
+      props.selectFeature(layer.feature.properties)
+    }
+
+    //if geojson is valid, add to map object
+    if(GJV.valid(geojson)){
+      console.log('cartodbData.geojson is a Valid geojson: ', geojson)
+
+      this.state.waterQualityLayer = L.geoJson(geojson, {
+        onEachFeature: function(feature, layer) {
+          layer.on({
+            click: onClick
+          })
+        },
+        pointToLayer: function(feature, latlng) {
+          return L.circleMarker(latlng, geojsonStyle)
+        }
+      }).addTo(this.state.map)
+    }
+
+    else {
+      console.log('cartodbData.geojson is an Invalid geojson: ', geojson)}
+  },
+  render() {
     return (
       <div className='layout-map cf'></div>
     )
