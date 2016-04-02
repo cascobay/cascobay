3b0582dd5495f3a5ae14477242be1fb4ac68c604
bfreeds <brandynfriedly@gmail.com>
2016-04-02 11:42:18 -0400

    update constants to reflect changes in actions

--- source/js/constants/index.js
+++ source/js/constants/index.js
@@ -1,19 +1,16 @@
 
 // Action Types
+export const ENTER_MAP = 'ENTER_MAP'
 export const REQUEST_DATA = 'REQUEST_DATA'
 export const RECEIVE_DATA = 'RECEIVE_DATA'
 export const SELECT_FEATURE = 'SELECT_FEATURE'
 
-export const VIEW_STORY = 'VIEW_STORY'
-export const VIEW_EXPLORE = 'VIEW_EXPLORE'
+export const SELECT_VIEW='SELECT_VIEW'
 
 export const NEXT_SLIDE = 'NEXT_SLIDE'
 export const PREVIOUS_SLIDE = 'PREVIOUS_SLIDE'
 
-// Sidebar Filters
-export const INTERPRETATION_FILTER = 'interpretation_filter'
-export const DATA_FILTER = 'data_filter'
-
-// App Modes
-export const STORY_MODE = 'STORY_MODE'
-export const EXPLORE_MODE = 'EXPLORE_MODE'
+// Sidebar Options
+export const LANDING_PAGE = 'LANDING_PAGE'
+export const DATA_VIEW = 'DATA_VIEW'
+export const INTERPRETATION_VIEW = 'INTERPRETATION_VIEW'
