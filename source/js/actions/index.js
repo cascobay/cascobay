b97b17934a8beff59c5358676e386c48f1492fe6
bfreeds <brandynfriedly@gmail.com>
2016-04-02 10:48:57 -0400

    remove unused actions, add selectView, enterMap action creators

--- source/js/actions/index.js
+++ source/js/actions/index.js
@@ -2,21 +2,25 @@ require('es6-promise').polyfill();
 import fetch from 'isomorphic-fetch'
 
 import {
+  ENTER_MAP,
   REQUEST_DATA,
   RECEIVE_DATA,
   SELECT_FEATURE,
-  VIEW_STORY,
-  VIEW_EXPLORE,
+  SELECT_VIEW,
   NEXT_SLIDE,
   PREVIOUS_SLIDE,
-  INTERPRETATION_FILTER,
-  DATA_FILTER,
-  STORY_MODE,
-  EXPLORE_MODE
+  SELECT_WINDOW
 } from '../constants'
 
 
 // Action Creators
+
+export function enterMap(mode) {
+  return {
+    type: ENTER_MAP,
+    mode
+  }
+}
 
 export function requestData(query) {
   return {
@@ -52,10 +56,18 @@ export function getCartodbData(username, query) {
       .then(geojson => dispatch(receiveData(username, query, geojson)))
   }
 }
+
 export function selectFeature(feature) {
   return {
     type: SELECT_FEATURE,
     feature
+  }
+}
+
+export function selectView(view) {
+  return {
+    type: SELECT_VIEW,
+    view
   }
 }
 export function viewStory() {
