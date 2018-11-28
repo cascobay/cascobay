fcd8829f7ae7c9cb7ac9ac30a26ab9c010fe4f9e
sarahllyman <slyman@cascobay.org>
2018-11-28 13:34:46 -0500

    Update header.jsx

--- source/js/components/header.jsx
+++ source/js/components/header.jsx
@@ -10,7 +10,7 @@ const Header = React.createClass({
         <header className='header layout-header'>
             <div className='logo'>
               <a className='logo-img' href='http://www.cascobay.org/'>
-                <img id ='logo' src='http://www.cascobay.org/wp-content/uploads/2014/05/FOCB-logo_FINAL-300x78.png' alt='Friends of Casco Bay'></img>
+                <img id ='logo' src='http://cascobay.org/wp-content/uploads/2017/02/cropped-FOCB-logo_FINAL_cmyk.png' alt='Friends of Casco Bay'></img>
               </a>
             </div>
           <h1 className='title'>Casco Bay Health Index</h1>
